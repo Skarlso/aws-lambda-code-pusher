79a85f7017e8ad276fddec5115abaa29500475ea
Gergely Brautigam <skarlso777@gmail.com>
2017-11-20 07:36:20 +0100

    Wrapped in a Handler.

--- index.py
+++ index.py
@@ -8,10 +8,14 @@ import git
 
 # First create a Github instance:
 TOKEN = os.environ.get("GITHUB_TOKEN")
-source = git.Repo.clone_from('https://' + TOKEN + '@github.com/Skarlso/blogsource.git', os.path.join(os.getcwd(), 'blog'))
 
-# Pull S3 artifact here and apply it to blog folder
+def handler(event, context):
+    source = git.Repo.clone_from('https://' + TOKEN + '@github.com/Skarlso/blogsource.git', os.path.join(os.getcwd(), 'blog'))
 
-source.git.add(A=True)
-source.index.commit('Added new content.')
-source.git.push()
+    # Pull S3 artifact here and apply it to blog folder
+
+    source.git.add(A=True)
+    source.index.commit('Added new content.')
+    source.git.push()
+    return None
+
